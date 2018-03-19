26d183cb585dc6c4cd6fe4f816e4809b2afe5586
kenZino <kennethli126@gmail.com>
2018-03-19 15:59:07 -0700

    Report model initial commit

---
+++ LowerMainlandYachtClub/Models/Report.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LowerMainlandYachtClub.Models
+{
+    public class Report
+    {
+        [Key]
+        public string ReportID { get; set; }
+        public string Content { get; set; }
+        public double Hours { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+        public string UserId { get; set; }
+
+
+
+    }
+}
